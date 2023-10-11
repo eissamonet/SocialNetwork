c1ee8853804cd828e54cd23b6774a494a0e5a203
Eissa Purnell <eissamonet@gmail.com>
2023-10-11 16:08:41 -0500

    user adn thought model route required

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// require the User and Thought models
+const thoughts = require('./thoughts');
+const users = require('./users');
+
+// export the User and Thought models
+module.exports = { thoughts, users };
